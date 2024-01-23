3d81b5a2aada3cc4292efb2c37e86d130b3a397b
Nishu Goel <n.goel@epilot.cloud>
2024-01-23 01:37:23 +0100

    Switch saveFile endpoint to v2

--- examples/resources/epilot-file_file/provider.tf
+++ examples/resources/epilot-file_file/provider.tf
@@ -2,11 +2,18 @@ terraform {
   required_providers {
     epilot-file = {
       source  = "epilot-dev/epilot-file"
-      version = "0.1.0"
+      version = "2.0.2"
     }
   }
 }
 
+variable "epilot_auth" {
+  type        = string
+  description = "epilot_auth"
+}
+
+
 provider "epilot-file" {
   # Configuration options
+  epilot_auth = var.epilot_auth
 }
