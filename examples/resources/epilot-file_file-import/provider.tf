9143363fcd092a644f2f6e94a706232b44fbbd47
Viljami <v.kuosmanen@epilot.cloud>
2024-08-13 20:38:35 +0200

    Add import example

---
+++ examples/resources/epilot-file_file-import/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    epilot-file = {
+      source  = "epilot-dev/epilot-file"
+      version = "2.1.1"
+    }
+  }
+}
+
+variable "epilot_auth" {
+  type        = string
+  description = "epilot_auth"
+}
+
+
+provider "epilot-file" {
+  # Configuration options
+  epilot_auth = var.epilot_auth
+  server_url  = "https://file.dev.sls.epilot.io"
+}
