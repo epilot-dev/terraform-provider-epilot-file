0d11282f564a9ff4169e7dbf55ea41022b1921a7
Viljami <v.kuosmanen@epilot.cloud>
2024-08-14 08:35:44 +0200

    Update goreleaser

--- examples/resources/epilot-file_file-import/resource.tf
+++ examples/resources/epilot-file_file-import/resource.tf
@@ -7,11 +7,11 @@
 # }
 
 # epilot-file_file.my_file:
-resource "epilot-file_file" "my_file" {
-    access_control = "private"
-    filename       = "8z87al.jpg"
-    mime_type      = "image/jpeg"
-    source_url     = "https://file.dev.sls.epilot.io/v1/files/public/links/739224-taqDBUyNjSX7c_n2v6Zvv/8z87al.jpg"
-    title          = "8z87al.jpg"
-    type           = "image"
-}
+# resource "epilot-file_file" "my_file" {
+#     access_control = "private"
+#     filename       = "8z87al.jpg"
+#     mime_type      = "image/jpeg"
+#     source_url     = "https://file.dev.sls.epilot.io/v1/files/public/links/739224-taqDBUyNjSX7c_n2v6Zvv/8z87al.jpg"
+#     title          = "8z87al.jpg"
+#     type           = "image"
+# }
